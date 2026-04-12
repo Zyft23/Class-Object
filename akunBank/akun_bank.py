68fc29367c557154f3e2e6da31d2b2ea5f0cf6d0
Zyft23 <ariazulvanrc@gmail.com>
2026-04-12 09:56:25 +0000

    tes1

--- akunBank/akun_bank.py
+++ akunBank/akun_bank.py
@@ -1,23 +1,38 @@
+
 class AkunBank:
-  # def __init__(self, nomor, pemilik, saldo_awal):
-  #   self.nomor = nomor 
-  #   self.pemilik = pemilik
-  #   self.saldo = saldo_awal
+  def __init__(self, nomor, pemilik, saldo_awal,riwayatsaldo):
+    self.nomor = nomor 
+    self.pemilik = pemilik
+    self.saldo = saldo_awal
+    self.riwayat = riwayatsaldo
   
   def cek_saldo(self):
     print(f"Saldo {self.pemilik}: Rp{self.saldo}")
     
   def tarik_tunai(self, jumlah):
-    if jumlah <= self.saldo:
+    if jumlah <= self.saldo:  
+      if jumlah <= 0:
+        print("jumlah tarik tunai harus lebih besar dari 0")
+        return
+      if jumlah > 1000000:
+        print("Jumlah tarik tunai melebihi batas maksimal Rp1.000.000")
+        return
       self.saldo -= jumlah
+      self.riwayat.append(f"{self.pemilik} menarik Rp{jumlah}")
       print(f"{self.pemilik} menarik Rp{jumlah}")
     else:
       print("Saldo tidak cukup!")
+      
   
   def transfer(self, tujuan, jumlah):
     if self.saldo >= jumlah:
+      if jumlah <= 0:
+        print("jumlah transfer harus lebih besar dari 0")
+        return
       self.saldo -= jumlah
+      self.saldo -= 2500
       tujuan.saldo += jumlah
+      self.riwayat.append(f"{self.pemilik} transfer Rp{jumlah} ke {tujuan.pemilik}")
       print(f"Transfer Rp{jumlah} ke {tujuan.pemilik} Berhasil!")
     else:
       print("Transfer Gagal: Saldo tidak cukup.")
--- sistemKasir/sistem_kasir.py
+++ sistemKasir/sistem_kasir.py
@@ -1,31 +1,63 @@
+member = True
 class Produk:
-  def __init__(self, nama, harga):
+  def __init__(self, nama, harga, stok):
     self.nama = nama
     self.harga = harga
+    self.stok = stok
     
 class Keranjang:
-  def __init__(self):
-    self.daftar_barang = []
+  def __init__(self, ):
+    self.daftar_barang = [] 
   
-  def tambah_produk(self, produk_baru):
-    self.daftar_barang.append(produk_baru)
-    print(f"Berhasil menambah: {produk_baru.nama}")
-    
+  def tambah_produk(self, produk_baru, jumlah=1,):
+    if produk_baru.stok < jumlah:
+      print(f"Stok {produk_baru.nama} tidak cukup. Stok tersedia: {produk_baru.stok}")
+      return
+    for _ in range(jumlah):
+      self.daftar_barang.append(produk_baru)
+    print(f"Berhasil menambah: {produk_baru.nama} (x{jumlah}) ke keranjang.")
+  
+  def hapus_produk(self, produk_hapus):
+    if produk_hapus in self.daftar_barang:
+      self.daftar_barang.remove(produk_hapus)
+      print(f"Berhasil menghapus: {produk_hapus.nama} dari keranjang.")
+    else:
+      print(f"{produk_hapus.nama} tidak ditemukan di keranjang.")
+  
   def hitung_total(self):
     total = 0
     for barang in self.daftar_barang:
       total += barang.harga
     return total
   
+  def bayar(self, jumlah_bayar, kembalian):
+    total_belanja = self.hitung_total()
+    if jumlah_bayar >= total_belanja:
+      print(f'berhasil bayar Rp{jumlah_bayar}')
+    else:
+      print('kurang duit')
+    if jumlah_bayar > total_belanja:
+      kembalian = jumlah_bayar - total_belanja
+      print(f'kembalian: Rp{kembalian}')
+  
   def cetak_struk(self):
     print("\n=== Struk Belanja ===")
     for barang in self.daftar_barang:
       print(f"- {barang.nama} : Rp{barang.harga}")
       
-    total_akhir = self.hitung_total() #293000
-    if total_akhir > 100000:
+    total_akhir = self.hitung_total()
+    if member is True and total_akhir >= 100000:
+      diskon = total_akhir * 0.15
+      print(f"\nDiskon (15%) \t: -Rp{diskon}")
+      total_akhir -= diskon
+    elif member is False and total_akhir >= 100000:
       diskon = total_akhir * 0.1
       print(f"\nDiskon (10%) \t: -Rp{diskon}")
       total_akhir -= diskon
-      
+    else:      
+      print("\nTidak ada diskon")
+    ppn = total_akhir * 0.11
+    print(f"PPN (11%) \t: Rp{ppn}")
+    total_akhir += ppn
     print(f"Total Akhir \t: Rp{total_akhir}")
+    
